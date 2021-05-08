0263e38ab44063b5d7342cdc8013535d33cbb210
RafaGomezGlez <A01733983@itesm.mx>
2021-05-07 23:35:18 -0500

    The chart changes do to the number of items specified #11

--- sort/sort.js
+++ sort/sort.js
@@ -1,35 +1,3 @@
-function linechart() {
-    const ctx = document.getElementById('myChart').getContext('2d');
-    const xlabels = [];
-    const myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: generateLabelsFromTable(100),
-            datasets: [{
-                label: '# of Votes',
-                data: randomData(100),
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-       
-    });
-}
 
 function randomData(quantity) {
     let arr = []
@@ -51,4 +19,25 @@ function generateLabelsFromTable(quantity)
         quantity --;
     }
     return labels;
+}
+
+function updateChart() {
+
+    var sortValue = parseInt(document.getElementById("sort-method").value);
+    var quantityValue = document.getElementById("quantityNumbers").value;
+    var delayValue = document.getElementById("delay").value;
+
+    var sortData = {
+        sort: sortValue,
+        quantity: quantityValue,
+        delay: delayValue,
+    };
+    myChart.data.labels = generateLabelsFromTable(quantityValue);
+    myChart.data.datasets.forEach((dataset) => {
+        dataset.data = (randomData(quantityValue));
+    });
+
+    console.log(myChart.data)
+    myChart.update();
+    
 }
