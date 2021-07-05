3464afda7605d6dcbf8ca2284db7b1b5d23a4166
Fernando Cuervo <63762598+Fcuervo21@users.noreply.github.com>
2021-07-05 17:23:10 -0500

    Improve bubble sort algorithm, color features #27

--- sort/chartjsCanvas.js
+++ sort/chartjsCanvas.js
@@ -1,12 +1,11 @@
 var ctx = document.getElementById('myChart');
-            var myChart = new Chart(ctx, {
+var myChart = new Chart(ctx, {
                 type: 'bar',
                 data: {
-                    labels: [12, 19, 3, 5, 2, 3],
-                    // labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                    labels: ['-', 'S', 'O', 'R', 'T', '-'],
                     datasets: [{
                         label: 'Value',
-                        data: [12, 19, 3, 5, 2, 3],
+                        data: [1, 1, 1, 1, 1, 1],
                         backgroundColor: [
                             'rgba(255, 99, 132, 0.2)',
                             'rgba(54, 162, 235, 0.2)',
@@ -14,14 +13,9 @@ var ctx = document.getElementById('myChart');
                             'rgba(75, 192, 192, 0.2)',
                             'rgba(153, 102, 255, 0.2)',
                             'rgba(255, 159, 64, 0.2)'
-                        ],
+                        ], 
                         borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
+                            'rgba(0, 0, 0, 1)'
                         ],
                         borderWidth: 2
                     }],
@@ -31,14 +25,22 @@ var ctx = document.getElementById('myChart');
                     scales: {
                         x: {
                             display: true,
+                            ticks: {
+                                font: {
+                                    size: 20,
+                                }
+                            }
                          },
                          y: {
                             display: false,
-                         }
+                         },
                     },
                     plugins: {
                         legend: {
-                            display: false
+                            display: false,
+                            font: {
+                                size: 30,
+                            }
                         }
                     }
                 }
--- sort/sort.js
+++ sort/sort.js
@@ -1,61 +1,74 @@
 
-function swap(arr, i) {
-    let tmp = arr[i];
-    arr[i] = arr[i + 1];
-    arr[i + 1] = tmp;
+function swap(arr, j) {
+    let tmp = arr[j];
+    arr[j] = arr[j + 1];
+    arr[j + 1] = tmp;
+}
+function updateColors(arr, j) {
+    arr[j] = 'rgb(217, 2, 250)';
+    arr[j + 1] = 'rgb(217, 2, 250)';
+}
+function updateColorsBefore(arr, j) {
+    arr[j] = 'rgb(255, 197, 82)';
+    arr[j + 1] = 'rgb(255, 197, 82)';
 }
 
-function updateChartDelayed(labels, data, colors, timeout) {
-    setTimeout(() => {
-      myChart.data.labels = labels;
-      myChart.data.datasets[0].data = data;
-      myChart.data.datasets[0].backgroundColor = colors;
-      myChart.update();
-    }, timeout);
-  }
+function updateChartDelayed(labels, data, colors) {
+            myChart.data.labels = labels;
+            myChart.data.datasets[0].data = data;
+            myChart.data.datasets[0].backgroundColor = colors;
+            myChart.update();
+}
 
 function randomData(quantity) {
     let arr = []
     while (quantity > 0) {
-        randomValue = Math.floor(Math.random() * 200);
+        randomValue = (Math.floor(Math.random() * 200)) + 1;
         arr.push(randomValue);
         quantity--;
     }
     return arr;
 }
 
-function generateLabelsFromTable(quantity)
+function generateLabelsFromTable(quantity, valoresDespues)
 {                       
     let labels = [];
-    iterator = 0;
-    while (quantity > 0) {
-        labels.push(iterator)
-        iterator ++;
-        quantity --;
+    for (let i = 0; i < quantity; i++){
+        labels.push(valoresDespues[i]);
     }
+    console.log(labels);
     return labels;
 }
 
 function sendData() {
-    // 
+    // Selected sorting method
     var sortValue = (document.getElementById("sort-method").value);
-    //
+    // Selected quantity of numbers
     var quantityValue = document.getElementById("quantityNumbers").value;
     var delayValue = document.getElementById("delay").value;
-
-
-    myChart.data.labels = generateLabelsFromTable(quantityValue);
+    //Generate random data
     myChart.data.datasets.forEach((dataset) => {
         dataset.data = (randomData(quantityValue));
     });
-
+    //Save random data
+    let generatedValues = myChart.data.datasets[0].data
+    // Set "generatedValues" to data labels
+    myChart.data.labels = generateLabelsFromTable(quantityValue, generatedValues);
+    // Update cahrt bar colors to yellow
+    myChart.data.datasets[0].backgroundColor = ['rgb(255, 197, 82)',
+                                                'rgb(255, 197, 82)',
+                                                'rgb(255, 197, 82)',
+                                                'rgb(255, 197, 82)',
+                                                'rgb(255, 197, 82)',
+                                                'rgb(255, 197, 82)'];
+    //Update
     myChart.update();
-
     switch (sortValue) {
         case 'insertionSort':
             setTimeout(()=> insertionSort(), 3000)
             break;
         case 'bubbleSort':
+            console.log("Todo bien");
             setTimeout(() => bubbleSort(), 3000)
             break
         case 'mergeSort':
@@ -69,26 +82,44 @@ function sendData() {
     
 }
 
-function bubbleSort() {  
+async function bubbleSort() {  
     var labels = myChart.data.labels;
     var data = myChart.data.datasets[0].data;
     var colors = myChart.data.datasets[0].backgroundColor;
-    var swapped;
-    var timeout = 0;
-    do {
-      swapped = false;
-      for (var i = 0; i < data.length; i++) {
-        if (data[i] > data[i + 1]) {        
-          swap(labels, i);
-          swap(data, i);
-          swap(colors, i);
-          timeout += 500;
-          this.updateChartDelayed(labels.slice(0), data.slice(0), colors.slice(0), timeout);
-          swapped = true;
+    for(let i = 0; i < data.length; i++) {
+        for(let j = 0; j < data.length - i - 1; j++) {
+            //Update bg color with the values compared
+            updateColors(colors, j);
+            updateChartDelayed(labels.slice(0), data.slice(0), colors.slice(0));
+            //Wait 1 second
+            await new Promise((resolve) =>
+                setTimeout(() => {
+                    resolve();
+                }, 1500)
+            );
+            //Compare and update
+            if (data[j] > data[j + 1]) {
+                swap(labels, j);
+                swap(data, j);
+                swap(colors, j);
+                updateChartDelayed(labels.slice(0), data.slice(0), colors.slice(0));
+                //Wait half a secnd
+                await new Promise((resolve) =>
+                setTimeout(() => {
+                    resolve();
+                }, 500)
+                );
+            
+            }
+            // Change bg color to previous values
+            updateColorsBefore(colors, j);
+            updateChartDelayed(labels.slice(0), data.slice(0), colors.slice(0));
         }
-      }
-    } while (swapped);
-  }
+        // Change last bar to green and update
+        colors[data.length - i - 1] = 'rgb(0, 255, 0)';
+        updateChartDelayed(labels.slice(0), data.slice(0), colors.slice(0));
+    }
+}
 
 function mergeSort(){
     console.log("mergeSort")
@@ -97,3 +128,4 @@ function mergeSort(){
 function quickSort(){
     console.log("quickSort")
 }
+
