3a63c97404a29e85e43f05d786c62becb7462654
Fernando Cuervo <63762598+Fcuervo21@users.noreply.github.com>
2021-07-21 16:11:50 -0500

    Merge Sort not finished, Projects page now will be Main #16

--- Projects/sort/sort.js
+++ Projects/sort/sort.js
@@ -84,7 +84,8 @@ function sendData() {
             setTimeout(() => bubbleSort(), 3000)
             break
         case 'mergeSort':
-            setTimeout(()=> mergeSort(), 3000)
+             let data = myChart.data.datasets[0].data;
+            setTimeout(()=> mergeSort(data, 0, data.length-1), 1000)
             break
         case 'quickSort':
             setTimeout(()=> quickSort(), 3000)
@@ -133,8 +134,87 @@ async function bubbleSort() {
     }
 }
 
-function mergeSort(){
-    console.log("mergeSort")
+async function merge(arr, l, m, r){
+    let labels = myChart.data.labels;
+    let colors = myChart.data.datasets[0].backgroundColor;
+    var n1 = m - l + 1;
+    var n2 = r - m;
+ 
+    // Create temp arrays
+    var L = new Array(n1);
+    var R = new Array(n2);
+ 
+    // Copy data to temp arrays L[] and R[]
+    for (var i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (var j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+ 
+    // Merge the temp arrays back into arr[l..r]
+ 
+    // Initial index of first subarray
+    var i = 0;
+ 
+    // Initial index of second subarray
+    var j = 0;
+ 
+    // Initial index of merged subarray
+    var k = l;
+ 
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            colors[k] = 'rgb(0, 0, 255)';
+            updateChartDelayed(labels.slice(0), arr.slice(0), colors.slice(0));
+            arr[k] = L[i];
+            labels[k] = L[i]
+            i++;
+        }
+        else {
+            colors[k] = 'rgb(0, 0, 255)';
+            updateChartDelayed(labels.slice(0), arr.slice(0), colors.slice(0));
+            arr[k] = R[j];
+            labels[k] = R[i];
+            j++;
+        }
+        k++;
+    }
+ 
+    // Copy the remaining elements of
+    // L[], if there are any
+    while (i < n1) {
+        colors[k] = 'rgb(255, 0, 0)';
+        updateChartDelayed(labels.slice(0), arr.slice(0), colors.slice(0));
+        arr[k] = L[i];
+        labels[k] = L[i];
+        i++;
+        k++;
+    }
+ 
+    // Copy the remaining elements of
+    // R[], if there are any
+    while (j < n2) {
+        colors[j] = 'rgb(0, 255, 0)';
+        updateChartDelayed(labels.slice(0), arr.slice(0), colors.slice(0));
+        arr[k] = R[j];
+        labels[k] = R[j];
+        j++;
+        k++;
+    }
+    console.log(arr);
+    updateChartDelayed(labels.slice(0), arr.slice(0), colors.slice(0));
+}
+
+async function mergeSort(data, acumulador, r){
+    if(acumulador >= r){
+        return;
+    }
+    let m = acumulador+ parseInt((r-acumulador)/2);
+    // console.log(`valorCero: ${acumulador} + r: ${r} - ${acumulador} /2`);
+    // console.log(m);
+
+    mergeSort(data,acumulador,m);
+    mergeSort(data,m+1,r);
+    merge(data, acumulador, m, r);
 }
 
 function quickSort(){
@@ -142,9 +222,9 @@ function quickSort(){
 }
 
 async function insertionSort(){
-    var labels = myChart.data.labels;
-    var data = myChart.data.datasets[0].data;
-    var colors = myChart.data.datasets[0].backgroundColor;
+    let labels = myChart.data.labels;
+    let data = myChart.data.datasets[0].data;
+    let colors = myChart.data.datasets[0].backgroundColor;
 
     // Green color to the first bar
     colors[0] = 'rgb(0, 255, 0)';
